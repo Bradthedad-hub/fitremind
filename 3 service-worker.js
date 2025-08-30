a7d4025da5b2a16e54f89b22e6c2b6d4a6ab1035
Bradthedad-hub <MatthiasRoos@gmx.net>
2025-08-30 12:48:12 +0200

    Create 3 service-worker.js

---
+++ 3 service-worker.js
@@ -0,0 +1,31 @@
+// Service Worker – Offline-Cache für FitRemind v4
+const CACHE = "fitremind-cache-v4";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
